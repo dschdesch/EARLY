f96b332a98a8e42d7603aa33032f8c7111dbd23a
Agustina <agustina.lagrecas@gmail.com>
2017-10-03 13:16:12 +0200

    textwrite

--- vs10/DataAnalysis/strfun/textwrite.m
+++ vs10/DataAnalysis/strfun/textwrite.m
@@ -3,6 +3,15 @@ function textwrite(fid, TXT);
 
 Nline = size(TXT,1);
 for iline=1:Nline,
-   lin = deblank(TXT(iline,:));
-   fprintf(fid, '%s\n', lin);
+   lin = deblank(TXT(iline,:)); 
+   %fprintf(fid, '%s\n', lin);  % commented by Hsin-Wei on 03.Oct.17
+   %% following added by Hsin-Wei on 03.Oct.17
+   fileID = fopen(fid,'a'); % convert fid from string to a file ID so that fprintf can write text to the file
+   try
+       fprintf(fileID,'%s\n', lin); % somehow lin sometimes becomes a CELL and causes error in fprintf
+   catch err
+       lin = cell2words(lin);       % convert lin from CELL to CHAR so that fprintf can work
+       fprintf(fileID, '%s\n', lin);
+   end
+   fclose(fileID);
 end
